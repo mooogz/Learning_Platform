c902eff66ad76cacd7068cf00936e0a58a951751
mooogz <meganm.mccaw@gmail.com>
2025-12-02 17:42:39 -0500

    commit

--- LP_app/Program.cs
+++ LP_app/Program.cs
@@ -1,6 +1,32 @@
 using LP_app.Components;
+using LP_app.Data;
+using LP_app.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? "Data Source=learning_platform.db";
+
+builder.Services.AddDbContext<LearningPlatformContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Add API Controllers
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add CORS for API access
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -8,15 +34,34 @@ builder.Services.AddRazorComponents()
 
 var app = builder.Build();
 
+// Apply pending migrations, create database, and seed data
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<LearningPlatformContext>();
+    dbContext.Database.Migrate();
+    await SeedDataService.SeedAsync(dbContext);
+}
+
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+
+// Map API Controllers
+app.MapControllers();
 
 app.UseAntiforgery();
 
